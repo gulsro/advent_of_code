114daa07e43e028da2b834c185e2c9990c3488b4
gulser <glserozkan@gmail.com>
2022-12-06 22:29:21 +0100

    :|

--- 2022/day5/day5.py
+++ 2022/day5/day5.py
@@ -11,20 +11,43 @@
 with open('input', 'r') as f:
     lines = f.read().splitlines()
 
-stack_list = [[], [[R], [P], [C], [D], [B]], [[H], [V], [G]], [[N], [S], [Q], [D], [J]], [[], [], [], [], []],  
-#directive_list = []
-#for i in range(len(lines)):
-#    directive_list.append(lines[])
+QUANTITY = 0
+FROM = 1
+TO = 2
+
+stack_list = [[], 
+              [['R'], ['P'], ['C'], ['D'], ['B'], ['G']], 
+              [['H'], ['V'], ['G']], 
+              [['N'], ['S'], ['Q'], ['D'], ['J'], ['P'], ['M']], 
+              [['P'], ['S'], ['L'], ['G'], ['D'], ['C'], ['N'], ['M']], 
+              [['J'], ['B'], ['N'], ['C'], ['P'], ['F'], ['L'], ['S']],
+              [['Q'], ['B'], ['D'], ['Z'], ['V'], ['G'], ['T'], ['S']],
+              [['B'], ['Z'], ['M'], ['H'], ['F'], ['T'], ['Q']],
+              [['C'], ['M'], ['D'], ['B'], ['F']], 
+              [['F'], ['C'], ['Q'], ['G']]]
+
 for i in range(len(lines)):
     lines[i] =  lines[i].split()
     lines[i].remove(lines[i][0])
     lines[i].remove(lines[i][1])
     lines[i].remove(lines[i][2])
-print(lines)
-print(len(lines))
+    #_, q, _, f, _, t = line.split() 
 
-#for i in range(len(lines)):
-#    for j in range(0, len(lines[i]), 2):
-#        lines[i].remove(lines[i][j])
+def move_crate(line, stack_list):
+    how_many = int(line[QUANTITY])
+    from_where = int(line[FROM])
+    to_where = int(line[TO])
+    #popped = stack_list[lines[1]].pop(-1)
+    for i in range(how_many):
+        elem = stack_list[from_where].pop(-1)
+        stack_list[to_where].append(elem)
 
-print(lines)
+for line in lines:
+    move_crate(line, stack_list)
+
+print(len(stack_list))
+    #print('-------')
+for stack in stack_list:
+    if stack:
+        print(stack[-1])
+
