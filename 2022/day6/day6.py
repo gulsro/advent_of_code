21ddad392f49a31c024349d99d2cc3ebe0c58933
gulser <glserozkan@gmail.com>
2022-12-06 18:24:55 +0100

    done, better

--- 2022/day6/day6.py
+++ 2022/day6/day6.py
@@ -2,19 +2,14 @@ with open('input', 'r') as f:
     inpt = f.read()
 
 set_list = []
-for i in range(0, len(inpt), 4):
+for i in range(len(inpt)):
     set_list.append(set(inpt[i:(i+4)]))
 
-sum_list = []        
+count = 4 #first check for 4-char-set
 for i in range(len(set_list)):
-    sum_list.append(len(set_list[i]))
-    if len(set_list[i]) == 4:  
+    #sum_list.append(len(set_list[i]))
+    if len(set_list[i]) == 4:
         print(i)
         break
-
-count = 0
-for i in range(len(sum_list)):
-    count += sum_list[i]
+    count+= 1
 print(count)
-
-#hrbbjkm
